e1dea820e32d8a5e220c29f05972ebccf2ab4684
AyushFullStackDevelloper <158497283+AyushFullStackDevellope@users.noreply.github.com>
2026-04-14 21:57:55 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -8,21 +7,19 @@ import RootNavigator from './src/navigation/RootNavigator';
 import { AuthProvider } from './src/contexts/AuthContext';
 import { ThemeProvider } from './src/contexts/ThemeContext';
 
-// Ignore specific third-party library deprecation warnings that are not actionable in current space
-LogBox.ignoreLogs(['InteractionManager has been deprecated']);
 
 function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <ThemeProvider>
-          <AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <SafeAreaProvider>
             <NavigationContainer>
               <RootNavigator />
             </NavigationContainer>
-          </AuthProvider>
-        </ThemeProvider>
-      </SafeAreaProvider>
+          </SafeAreaProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </GestureHandlerRootView>
   );
 }
